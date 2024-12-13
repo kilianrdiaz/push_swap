4688b0facf5f92c52753e0bde4d3f3bf8a5a869b
Kilian Royo Diaz <kilianrdiaz@gmail.com>
2024-12-13 01:04:14 +0100

    Add files via upload

--- sources/pairs.c
+++ sources/pairs.c
@@ -24,17 +24,42 @@ static void	pair_to_max(t_stack *a, t_stack *b)
 	}
 }
 
-static int	find_first_pair(t_stack *a, t_stack *pair)
+static void	pair_to_min(t_stack *a, t_stack *b)
 {
-	if (pair->value < a->value)
+	while (a != NULL)
 	{
-		a->pair = pair;
-		return (1);
+		if (a->value == get_min(a))
+		{
+			b->pair = a;
+			break ;
+		}
+		a = a->next;
 	}
-	return (0);
 }
 
-void	pair(t_stack *a, t_stack *b)
+static int	find_first_pair(t_stack *node, t_stack *pair, char id)
+{
+	if (id == 'a')
+	{
+		if (pair->value < node->value)
+		{
+			node->pair = pair;
+			return (1);
+		}
+		return (0);
+	}
+	else
+	{
+		if (pair->value > node->value)
+		{
+			node->pair = pair;
+			return (1);
+		}
+		return (0);
+	}
+}
+
+void	pair_a(t_stack *a, t_stack *b)
 {
 	t_stack	*tmp;
 
@@ -48,7 +73,7 @@ void	pair(t_stack *a, t_stack *b)
 		{
 			while (b != NULL)
 			{
-				if (find_first_pair(a, b))
+				if (find_first_pair(a, b, 'a'))
 					break ;
 				b = b->next;
 			}
@@ -60,5 +85,34 @@ void	pair(t_stack *a, t_stack *b)
 			}
 		}
 		a = a->next;
+	}
+}
+
+void	pair_b(t_stack *a, t_stack *b)
+{
+	t_stack	*tmp;
+
+	tmp = a;
+	while (b != NULL)
+	{
+		a = tmp;
+		if (b->value < get_min(a) || b->value > get_max(a))
+			pair_to_min(a, b);
+		else
+		{
+			while (a != NULL)
+			{
+				if (find_first_pair(b, a, 'b'))
+					break ;
+				a = a->next;
+			}
+			while (a != NULL)
+			{
+				if (b->value < a->value && a->value < b->pair->value)
+					a->pair = b;
+				a = a->next;
+			}
+		}
+		b = b->next;
 	}
 }
